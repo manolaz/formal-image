1ac3ec2625d154ff649f8877e4663230ba311e75
Anh Trung Nguyen Vu <trungupdate@gmail.com>
2018-09-12 15:45:45 +0700

    Photo compressor script

---
+++ photocomp.py
@@ -0,0 +1,24 @@
+import glob
+import pathlib    
+from PIL import Image
+mainfolder = input("Input folder: ") 
+
+p = pathlib.Path("{}/".format(mainfolder))
+
+def compress(img_file):
+    print(img_file)
+    basewidth = 1080
+    img = Image.open(img_file)
+    wpercent = (basewidth/float(img.size[0]))
+    hsize = int((float(img.size[1])*float(wpercent)))
+    img = img.resize((basewidth,hsize), Image.ANTIALIAS)
+    img.save(img_file)
+
+for img_file in p.rglob("*.png"):
+    compress(img_file)
+
+for img_file in p.rglob("*.jpg"):
+    compress(img_file)
+
+for img_file in p.rglob("*.jpeg"):
+    compress(img_file)
